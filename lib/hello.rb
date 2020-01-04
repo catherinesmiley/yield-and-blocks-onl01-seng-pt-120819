490d337a30c89d9200edccd450aac3509cf07c16
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-04 18:02:27 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -30,22 +30,22 @@
 
 
 
-def hello_t(array)
-  if block_given?
-    i = 0 
+# def hello_t(array)
+#   if block_given?
+#     i = 0 
     
-    while i < array.length
-      yield (array[i])
-      i += 1 
-    end
+#     while i < array.length
+#       yield (array[i])
+#       i += 1 
+#     end
   
-    array
-  else
-    puts "Hey! No block was given!"
-  end
+#     array
+#   else
+#     puts "Hey! No block was given!"
+#   end
   
   
-end
+# end
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
